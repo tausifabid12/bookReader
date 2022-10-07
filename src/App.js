e6178abcb40e2381dc707a95ca07b931cf8b768e
tausifabid12 <tasifabid12@gmail.com>
2022-10-07 20:31:44 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/route";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <RouterProvider router={router} />;
     </div>
   );
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
+import About from "../components/About";
+import Books from "../components/Books";
+import Layout from "../Layout/Layout";
+import BookDetails from "../components/BookDetails";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/books",
+        element: <Books />,
+        loader: () => fetch("https://api.itbook.store/1.0/new"),
+      },
+      {
+        path: "/book/:id",
+        element: <BookDetails />,
+        loader: ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+      },
+    ],
   },
 ]);
