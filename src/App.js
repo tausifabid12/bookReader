46cc24c7b6c16f3343b9011e101a7b2b041fcc87
tausifabid12 <tasifabid12@gmail.com>
2022-10-07 14:23:11 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/route";
+import Header from "./components/Header";
 
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return (
+    <div>
+      <Header />
+      <RouterProvider router={router} />;
+    </div>
+  );
 }
 
-export default App
+export default App;
